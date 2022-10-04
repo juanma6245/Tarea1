0bfb8d34ab0c0877b061276782b011316fd4cbfd
Juan Manuel Manga Hurtado <juanma6245@hotmail.com>
2022-10-04 22:58:56 +0200

    Commit 1

---
+++ src/Tarea1.java
@@ -0,0 +1,9 @@
+
+public class Tarea1 {
+
+	public static void main(String[] args) {
+		System.out.println("Paso 1");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author juanm
+ *
+ */
+module Tarea1 {
+}
